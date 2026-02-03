cd5a9a07aa1a6fa02e7c4e296b0c11bffb51a60a
J4yth31k <javonte.platt@gmail.com>
2026-02-03 08:59:26 -0600

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div style={{ color: 'white', background: 'black', height: '100vh' }}>
+      <h1>Netflix Clone Starter</h1>
+    </div>
+  )
+}
+
+export default App
