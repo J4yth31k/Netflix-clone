4a9a282e6d93f5066d504764391e361265c1ce6a
J4yth31k <javonte.platt@gmail.com>
2026-02-05 11:54:54 -0600

    Delete App.jsx

--- App.jsx
+++
@@ -1,9 +0,0 @@
-const App = () => {
-  return (
-    <div style={{ color: 'white', background: 'black', height: '100vh' }}>
-      <h1>Netflix Clone Starter</h1>
-    </div>
-  )
-}
-
-export default App
