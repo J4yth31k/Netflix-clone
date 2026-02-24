b729ca8809835969116ddd23c9c71cb47d4aea08
J4yth31k <javonte.platt@gmail.com>
2026-02-24 10:47:37 -0600

    Create main App component with AuthProvider

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { AuthProvider } from './context/AuthContext';
+import AppRouter from './AppRouter';
+
+const App = () => (
+  <AuthProvider>
+    <AppRouter />
+  </AuthProvider>
+);
+
+export default App;
